2649d3f6fc83ffe6bb95d286c1621c4449486f54
Rachel Hawa <rachel.hawa@gmail.com>
2019-02-17 20:40:47 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,12 @@
+require 'pry'
+require_relative "../config/environment.rb"
+
+class Dog
+  attr_accessor :name, :breed, :id
+
+  def initialize( id = nil, name, breed)
+    @id = id
+    @name = name
+    @breed = breed
+  end
+end
